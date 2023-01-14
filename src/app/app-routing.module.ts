7164db1929f19f7eb6bbbffa6e703d5aff6f0edb
Keneth Riera <kenethrr@outlook.com>
2023-01-13 21:56:32 -0500

    Estructura inicial del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/* Components */
+import { MainPageComponent } from 'src/app/components/main-page/main-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { PurchaseComponent } from './components/purchase/purchase.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    MainPageComponent,
+    PurchaseComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
