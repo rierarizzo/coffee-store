4ddff694701c745f0c1a6aeb9a4cbd4b0e621ff5
DaniloPV18 <damipive1809@gmail.com>
2023-01-15 01:38:08 -0500

    Ruta adm Usuarios add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 /* Components */
 import { MainPageComponent } from 'src/app/components/main-page/main-page.component';
+import { UsersListComponent } from './components/users/users-list/users-list.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
+  /* Rutas usuario */
+  {path: 'adm-usuarios', component: UsersListComponent}
 ];
 
 @NgModule({
