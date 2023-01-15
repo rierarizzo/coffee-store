c7b1dd9e7ffdb7a9e5beca96ed9c2cc808ba9241
Melmero <melanymero7@gmail.com>
2023-01-15 18:38:47 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 /* Components */
 import { MainPageComponent } from 'src/app/components/main-page/main-page.component';
 import { SignInComponent } from 'src/app/components/sign-in/sign-in.component';
+import { AddProductsComponent } from './components/Inventary/products/add-products/add-products.component';
+import { ProductsViewComponent } from './components/Inventary/products/products-view/products-view.component';
 import { UsersListComponent } from './components/users/users-list/users-list.component';
 
 const routes: Routes = [
@@ -11,7 +13,10 @@ const routes: Routes = [
   /* Authentication */
   { path: 'login', component: SignInComponent },
   /* Rutas usuario */
-  {path: 'adm-usuarios', component: UsersListComponent}
+  {path: 'adm-usuarios', component: UsersListComponent},
+  /* Rutas Producto */
+  {path: 'adm-productos/agregar', component: AddProductsComponent},
+  {path: 'adm-productos/view', component: ProductsViewComponent}
 ];
 
 @NgModule({
