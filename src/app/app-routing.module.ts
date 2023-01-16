725594ab50ccc9a7a35782fe6a6d252a40e8d542
Keneth Riera <kenethrr@outlook.com>
2023-01-16 01:33:22 -0500

    Se agrega validaciones en formularios para autenticacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 /* Components */
 import { MainPageComponent } from 'src/app/components/main-page/main-page.component';
 import { SignInComponent } from 'src/app/components/sign-in/sign-in.component';
+import { SignUpComponent } from 'src/app/components/sign-up/sign-up.component';
 import { AddProductsComponent } from './components/Inventary/products/add-products/add-products.component';
 import { ProductsViewComponent } from './components/Inventary/products/products-view/products-view.component';
 import { UsersListComponent } from './components/users/users-list/users-list.component';
@@ -12,11 +13,12 @@ const routes: Routes = [
   { path: '', component: MainPageComponent },
   /* Authentication */
   { path: 'login', component: SignInComponent },
+  { path: 'register', component: SignUpComponent },
   /* Rutas usuario */
-  {path: 'adm-users', component: UsersListComponent},
+  { path: 'adm-users', component: UsersListComponent },
   /* Rutas Producto */
-  {path: 'adm-productos/agregar', component: AddProductsComponent},
-  {path: 'adm-productos/view', component: ProductsViewComponent}
+  { path: 'adm-productos/agregar', component: AddProductsComponent },
+  { path: 'adm-productos/view', component: ProductsViewComponent }
 ];
 
 @NgModule({
