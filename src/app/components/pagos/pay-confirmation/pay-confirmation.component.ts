5f54ec99abef22a26dfc94c715ad500c9509894f
IvonneMinchalaP <108905265+IvonneMinchalaP@users.noreply.github.com>
2023-01-17 17:55:31 -0500

    Componente Pay-C

---
+++ src/app/components/pagos/pay-confirmation/pay-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-confirmation',
+  templateUrl: './pay-confirmation.component.html',
+  styleUrls: ['./pay-confirmation.component.css']
+})
+export class PayConfirmationComponent {
+
+}
