83d5d952fe1560ffad095eb04d40c8e036d2d689
Melmero <melanymero7@gmail.com>
2023-01-18 02:13:21 -0500

    add-form

--- src/app/components/Inventary/products/add-products/add-products.component.ts
+++ src/app/components/Inventary/products/add-products/add-products.component.ts
@@ -6,60 +6,61 @@ import { ProductsService } from 'src/app/services/products/products.service';
 import { Product } from 'src/app/entities/products';
 import { NavigationExtras, Router } from '@angular/router';
 import { ProductConfirmationComponent } from '../product-confirmation/product-confirmation.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-add-products',
   templateUrl: './add-products.component.html',
   styleUrls: ['./add-products.component.css']
 })
-export class AddProductsComponent {
+export class AddProductsComponent implements OnInit{
   productNew: any;
   formAdd!:FormGroup;
 
   constructor(private router: Router, @Inject(MAT_DIALOG_DATA) public data: any, 
   private dialogRef: MatDialogRef<AddProductsComponent>, private productsService: ProductsService, 
-  private formBuilder:FormBuilder, private dialog: MatDialog){
+  private formBuilder:FormBuilder, private dialog: MatDialog, private _snackBar: MatSnackBar){
   
   }
   ngOnInit(): void {
-
-  }
-
-  productNuevo = new FormGroup({
-    Codigo: new FormControl('', Validators.required),
-    Nombre: new FormControl('', Validators.required),
-    Precio: new FormControl('', Validators.required),
-    Categoria: new FormControl('', Validators.required),
-    Estado: new FormControl('', Validators.required),
-    Descripcion: new FormControl('', Validators.required)
+  this.formAdd = this.formBuilder.group({
+    Codigo: ['', Validators.required],
+    Nombre: ['', Validators.required],
+    Precio: ['', Validators.required],
+    Categoria: ['', Validators.required],
+    Estado:['', Validators.required],
+    Descripcion: ['', Validators.required],
+    Imagen:['', Validators.required]
   })
-
-  onSubmit() {    
-     let objToSend: NavigationExtras = {
-    queryParams: {
-      Codigo: this.productNuevo.value.Codigo,
-      Nombre: this.productNuevo.value.Nombre,
-      Precio: this.productNuevo.value.Precio,
-      Categoria: this.productNuevo.value.Categoria,
-      Estado: this.productNuevo.value.Estado,
-      Descripcion: this.productNuevo.value.Descripcion,
-    },
-    skipLocationChange: false,
-    fragment: 'top'
-  };
- 
-};
-redirecTo(uri: string) {
-  this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
-    this.router.navigate([uri]));
-}
-OpenProductAdd(element: any) {
-  this.dialog.open(AddProductsComponent, {
-    width: '50%',
-    data: {
-      Product: element
+  }
+   
+  productAdd(){
+    const conspro:Product={
+      Codigo: this.formAdd.value.Codigo,
+      Nombre: this.formAdd.value.Nombre,
+      Precio: this.formAdd.value.Precio,
+      Categoria: this.formAdd.value.Categoria,
+      Estado: this.formAdd.value.Estado,
+      Descripcion: this.formAdd.value.Descripcion,
+      Imagen: this.formAdd.value.Imagen
     }
-  });
-}
+    this._snackBar.open('El Producto fue agregado con éxito','',{
+      duration: 1500,
+      horizontalPosition: 'start',
+      verticalPosition: 'bottom',
+      })
+
+    this.router.navigate(['/'])
+    .then(()=>this.router.navigate(['/adm-productos/view'],{state:{editarDatos: this.productsService.AddProduct(conspro)}}))
+
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
+      this.dialogRef.close();
+  }
+  
+
 
 cancel() {
   this.dialogRef.close();
